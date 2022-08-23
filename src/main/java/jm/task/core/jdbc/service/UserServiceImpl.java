142ff9e0536ada8a1f1dee88df74bac744cdf53b
dineka89 <dineka89@inbox.ru>
2022-08-23 17:54:40 +0300

    UserDatabase test work from Hibernate

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -2,15 +2,13 @@ package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-
-    UserDao userDaoJDBC = new UserDaoJDBCImpl();
+    UserDao userDaoJDBC = new UserDaoHibernateImpl();
     public void createUsersTable() throws SQLException, ClassNotFoundException {
         userDaoJDBC.createUsersTable();
     }
