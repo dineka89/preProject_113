65d5889624ceeb460326c91c5c38546fe5d12a32
dineka89 <dineka89@inbox.ru>
2022-08-23 17:48:13 +0300

    UserDatabase test work from MySql after mentor comments fix

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+
     UserDao userDaoJDBC = new UserDaoHibernateImpl();
     public void createUsersTable() throws SQLException, ClassNotFoundException {
         userDaoJDBC.createUsersTable();
