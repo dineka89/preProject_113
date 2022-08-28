d84d7c271f3c0a0e27619375cedd45bf5f56738a
dineka89 <dineka89@inbox.ru>
2022-08-29 00:02:32 +0300

    Hibernate работает через properties

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -2,7 +2,6 @@ package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.io.IOException;
@@ -10,7 +9,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    UserDao userDaoJDBC = new UserDaoJDBCImpl();
+    UserDao userDaoJDBC = new UserDaoHibernateImpl();
     public void createUsersTable() throws SQLException, ClassNotFoundException, IOException {
         userDaoJDBC.createUsersTable();
     }
